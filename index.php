9207706bf78c27631d9745ce9f9ffce7459ab3b0
Elliot Londt <elliotlondtofficial@gmail.com>
2017-09-20 14:32:59 +1200

    Background image and nav-bar.

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/boostrap.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="C:\Users\DesperateForCars\Documents\GitHub\stoneandsteel\css\bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="C:\Users\DesperateForCars\Documents\GitHub\stoneandsteel\css\style.css">
     <title>Stone & Steel Realty</title>
 </head>
 <body>
@@ -19,6 +19,6 @@
     </nav>
     <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="C:\Users\DesperateForCars\Documents\GitHub\stoneandsteel\js\main.js"></script>
 </body>
 </html>
