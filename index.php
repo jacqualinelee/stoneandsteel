12caea18d900440d8f614cc549fa0491a0d8521c
Elliot Londt <elliotlondtofficial@gmail.com>
2017-09-20 14:57:33 +1200

    Added grids, background texture and cards.

--- index.php
+++ index.php
@@ -11,14 +11,46 @@
     <nav class="navbar navbar-default">
         <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand">
-                    <img alt="Stone & Steel Brand" src="">
+                <a class="navbar-brand" href="index.php">
+                    <img id="mylogo" alt="Stone & Steel">
                 </a>
-            </div>              
+            </div>    
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.php">Home</a></li>
+            </ul>          
         </div>
     </nav>
-    <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="C:\Users\DesperateForCars\Documents\GitHub\stoneandsteel\js\main.js"></script>
+    <div id="mca">
+        <div class="row">
+            <div class="col-md-3"><div class="card" style="width: 20rem;">
+              <img class="card-img-top" src="..." alt="Card image cap">
+              <div class="card-block">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div></div>
+        <div class="col-md-3"><div class="card" style="width: 20rem;">
+          <img class="card-img-top" src="..." alt="Card image cap">
+          <div class="card-block">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div></div>
+    <div class="col-md-3"><div class="card" style="width: 20rem;">
+      <img class="card-img-top" src="..." alt="Card image cap">
+      <div class="card-block">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div></div>
+<div class="col-md-3"></div>
+</div>
+</div>
+<script src="https://code.jquery.       com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="C:\Users\DesperateForCars\Documents\GitHub\stoneandsteel\js\main.js"></script>
 </body>
 </html>
