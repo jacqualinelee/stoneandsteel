404c2c972811c7e64b830181318843687631e83b
Elliot Londt <elliotlondtofficial@gmail.com>
2017-09-25 12:16:16 +1300

    Repo General Updates

--- index.php
+++ index.php
@@ -3,24 +3,26 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="C:\Users\DesperateForCars\Documents\GitHub\stoneandsteel\css\bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="C:\Users\DesperateForCars\Documents\GitHub\stoneandsteel\css\style.css">
+    <link rel="stylesheet" type="text/css" href="css\bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css\style.css">
     <title>Stone & Steel Realty</title>
 </head>
 <body>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="index.php">
-                    <img id="mylogo" alt="Stone & Steel">
-                </a>
-            </div>    
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="index.php">Home</a></li>
-            </ul>          
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="index.php">Stone & Steel Realty</a>
         </div>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="index.php">Home</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+      </div>
     </nav>
-    <div id="mca">
+<!--     <div id="mca">
         <div class="row">
             <div class="col-md-3"><div class="card" style="width: 20rem;">
               <img class="card-img-top" src="..." alt="Card image cap">
@@ -48,9 +50,9 @@
 </div></div>
 <div class="col-md-3"></div>
 </div>
-</div>
-<script src="https://code.jquery.       com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+</div> -->
+<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="C:\Users\DesperateForCars\Documents\GitHub\stoneandsteel\js\main.js"></script>
+<script src="js\main.js"></script>
 </body>
 </html>
